44fc48d13855df8fb3e7bf4de88d236b9e266cd8
Erwan Goalou <erwan.goalou@gmail.com>
2008-03-13 14:04:59 +0000

    optimize page loading

--- ocsreports/all_soft.php
+++ ocsreports/all_soft.php
@@ -1,5 +1,6 @@
 <?php
 require_once('require/function_table_html.php');
+require_once('require/function_config_generale.php');
 echo "<script language=javascript>
 		function garde_valeur(did,form_name,hidden_name){
 				document.getElementById(hidden_name).value=did;
@@ -8,47 +9,52 @@ echo "<script language=javascript>
 
 </script>";
 
-if ($_POST['RESET']) 
+if ($_POST['RESET']){ 
 unset($_POST['search']);
-
+unset($_POST['NBRE']);
+}
 
 //search all onglet
 if( $_SESSION["lvluser"] == ADMIN) {
-	$sql_list_alpha ="select substr(trim(name),1,1) alpha from softwares,accountinfo a 
+	$sql_list_alpha ="select substr(trim(name),1,1) alpha, name,count(*) nb from softwares,accountinfo a 
 						where ".$_SESSION["mesmachines"]."
-						and a.hardware_id=softwares.HARDWARE_ID ";
-	if (isset($_POST['search']) and $_POST['search'] != "")
-	$sql_list_alpha.=" and softwares.name like '%".$_POST['search']."%'";
-			$sql_list_alpha.=" and substr(trim(name),1,1) is not null order by 1";	
+						and a.hardware_id=softwares.HARDWARE_ID and";			
 }else{
-	$sql_list_alpha="select substr(trim(name),1,1) alpha
-						 from softwares_name_cache cache ";
-	$sql_list_alpha.=" where substr(trim(name),1,1) is not null";	
-	if (isset($_POST['search']) and $_POST['search'] != "")
-	$sql_list_alpha.=" and name like '%".$_POST['search']."%'";
+	$sql_list_alpha="select substr(trim(name),1,1) alpha,count(*)nb,name from softwares where";
 }
+if (isset($_POST['search']) and $_POST['search'] != "")
+	$sql_list_alpha.=" softwares.name like '%".$_POST['search']."%' and";
+$sql_list_alpha.=" substr(trim(name),1,1) is not null group by name ";	
+	if (isset($_POST['NBRE']) and $_POST['NBRE'] != "")
+	$sql_list_alpha.=" having nb ".$_POST['COMPAR']." ".$_POST['NBRE']." ";
+	$sql_list_alpha.=" order by 1";
 
-$result_list_alpha = mysql_query( $sql_list_alpha, $_SESSION["readServer"]);
- while($item_list_alpha = mysql_fetch_object($result_list_alpha)){
- 	if (strtoupper($item_list_alpha -> alpha) != "" 
-		and strtoupper($item_list_alpha -> alpha) != Ã
-		and strtoupper($item_list_alpha -> alpha) != Â
-		and strtoupper($item_list_alpha -> alpha) != Ä){
-			if (!isset($_POST['onglet_bis']))
-				$_POST['onglet_bis']=strtoupper($item_list_alpha -> alpha);
-			$list_alpha[strtoupper($item_list_alpha -> alpha)]=strtoupper($item_list_alpha -> alpha);
-			if (isset($first)){
-				$first=$list_alpha[strtoupper($item_list_alpha -> alpha)];				
-			}
- 	}
+//execute the query only if necessary 
+if($_SESSION['REQ_ONGLET_SOFT'] != $sql_list_alpha or !isset($_POST['onglet_bis'])){
+	$result_list_alpha = mysql_query( $sql_list_alpha, $_SESSION["readServer"]);
+ 	while($item_list_alpha = mysql_fetch_object($result_list_alpha)){
+ 		if (strtoupper($item_list_alpha -> alpha) != "" 
+			and strtoupper($item_list_alpha -> alpha) != Ã
+			and strtoupper($item_list_alpha -> alpha) != Â
+			and strtoupper($item_list_alpha -> alpha) != Ä){
+				if (!isset($_POST['onglet_bis']))
+					$_POST['onglet_bis']=strtoupper($item_list_alpha -> alpha);
+				$list_alpha[strtoupper($item_list_alpha -> alpha)]=strtoupper($item_list_alpha -> alpha);
+				if (!isset($first)){
+					$first=$list_alpha[strtoupper($item_list_alpha -> alpha)];				
+				}
+ 		}
+	}
+	
+	if (!isset($list_alpha[str_replace('\"','"',$_POST['onglet_bis'])])){
+		$_POST['onglet_bis']=$first;
+	}
+	$_SESSION['REQ_ONGLET_SOFT']= $sql_list_alpha;
+	$_SESSION['ONGLET_SOFT']=$list_alpha;
 }
-if (!isset($list_alpha[str_replace('\"','"',$_POST['onglet_bis'])])){
-$_POST['onglet_bis']=$first;
-}
-
 $form_name = "all_soft";
 echo "<form name='".$form_name."' id='".$form_name."' method='POST' action=''>";
- onglet($list_alpha,$form_name,"onglet_bis",20);
+ onglet($_SESSION['ONGLET_SOFT'],$form_name,"onglet_bis",20);
  if (!(isset($_POST["pcparpage"])))
  $_POST["pcparpage"]=20;
  echo "<table cellspacing='5' width='80%' BORDER='0' ALIGN = 'Center' CELLPADDING='0' BGCOLOR='#C7D9F5' BORDERCOLOR='#9894B5'><tr><td align=center>";
@@ -60,7 +66,8 @@ foreach( $machNmb as $nbm ) {
 }
 $pcParPageHtml .=  "</select></td></tr><tr><td align=center>";
 echo $pcParPageHtml;
-if (isset($_POST['search']) and $_POST['search'] != "")
+if ((isset($_POST['search']) and $_POST['search'] != "") or
+	((isset($_POST['NBRE']) and $_POST['NBRE'] != "")))
 echo "<font color=red size=3><b>".$l->g(767)."</b></font>";
 if (isset($_POST["pcparpage"])){
 	$limit=$_POST["pcparpage"];
@@ -68,27 +75,23 @@ if (isset($_POST["pcparpage"])){
 $fin_limit=$limit;
 	
 }
+//sql query for CSV export 
+$sql_csv="";
+$sql_filter_name="";
 if( $_SESSION["lvluser"] == ADMIN ) {
-	$reqCount="select  count(name) nb from softwares,accountinfo a 
-			where ".$_SESSION["mesmachines"];
-			if (isset($_POST['search']) and $_POST['search'] != "")
-	$reqCount.=" and name like '%".$_POST['search']."%' ";
-	$reqCount.=" and a.hardware_id=softwares.HARDWARE_ID
-				and name like '".$_POST['onglet_bis']."%'";
-
-
-$sql="select  name, count(name) nbre from softwares,accountinfo a 
+	$sql="select  name, count(name) nbre from softwares,accountinfo a 
 			where ".$_SESSION["mesmachines"]."
-				and a.hardware_id=softwares.HARDWARE_ID
-				and name like '".$_POST['onglet_bis']."%'";
-if (isset($_POST['search']) and $_POST['search'] != "")
-	$sql.=" and name like '%".$_POST['search']."%' ";
-$sql.="	group by name
-		order by 2 desc  limit ".$deb_limit.",".$fin_limit;
-	
-	
+				and a.hardware_id=softwares.HARDWARE_ID";
+	$sql_csv=$sql;
+	$sql_filter=" and name like '".$_POST['onglet_bis']."%'";
+	if (isset($_POST['search']) and $_POST['search'] != "")
+		$sql_filter_name=" and name like '%".$_POST['search']."%' ";
+	$sql_groupby="	group by name";
+	$sql.=$sql_filter.$sql_filter_name.$sql_groupby;
+	$sql_csv.=$sql_filter_name.$sql_groupby;	
 }else{
-	
+	//BEGIN use CACHE
+	if ($_SESSION["usecache"] == 1){
 	$search_soft="select name from softwares_name_cache 
 			where name like '".$_POST['onglet_bis']."%'";
 	if (isset($_POST['search']) and $_POST['search'] != "")
@@ -101,19 +104,32 @@ $sql.="	group by name
  	$list_soft=substr($list_soft,0,-2);
  	if ($list_soft == "")
  	$list_soft="''";
-	$reqCount="select count(distinct name) nb
-	from softwares
-	where NAME in (".$list_soft.")";
+
 	$sql="select name, count(name) nbre from softwares 
 			where name in (".$list_soft.")
-			group by name
-			order by 2 desc limit ".$deb_limit.",".$fin_limit;
-	
+			group by name";
+	//END use CACHE
+	}else{
+		$sql="select  name, count(name) nbre from softwares 
+			where name like '".$_POST['onglet_bis']."%'";
+		if (isset($_POST['search']) and $_POST['search'] != "")
+			$sql.=" and name like '%".$_POST['search']."%' ";
+		$sql.="	group by name";
+	}
+	$sql_csv="select name, count(name) nbre from softwares 
+			group by name";
+			
 }
+if (isset($_POST['NBRE']) and $_POST['NBRE'] != ""){
+	$sql.=" having nbre ".$_POST['COMPAR']." ".$_POST['NBRE']." ";
+	$sql_csv.=" having nbre ".$_POST['COMPAR']." ".$_POST['NBRE']." ";
+}
+$reqCount="select count(*) nb from (".$sql.") toto";
 
+$sql.="	order by 2 desc limit ".$deb_limit.",".$fin_limit;
 $resCount = mysql_query($reqCount, $_SESSION["readServer"]) or die(mysql_error($_SESSION["readServer"]));
 $valCount = mysql_fetch_array($resCount);
-
+$_SESSION["forcedRequest"]=$sql_csv;
 $result = mysql_query( $sql, $_SESSION["readServer"]);
 	$i=0;
 	while($colname = mysql_fetch_field($result)){
@@ -158,8 +174,15 @@ if ($nbpage >1){
 	echo "<img src='image/proch24.png' OnClick='garde_valeur(\"".$up."\",\"".$form_name."\",\"page\")'>";
 	
 }
-echo "<br><input type='input' name='search' value='".$_POST['search']."'>
-		<input type='submit' value='".$l->g(393)."'><input type='submit' value='".$l->g(396)."' name='RESET'>";
+echo "<br><table bgcolor='#66CCCC'><tr><td colspan=2 align=center >FILTRES</td></tr><tr><td align=right>".$l->g(382).": <input type='input' name='search' value='".$_POST['search']."'>
+				<td rowspan=2><input type='submit' value='".$l->g(393)."'><input type='submit' value='".$l->g(396)."' name='RESET'>
+		</td></tr><tr><td align=right>nbre <select name='COMPAR'>
+			<option value='<' ".($_POST['COMPAR'] == '<'?'selected':'')."><</option>
+			<option value='>' ".($_POST['COMPAR'] == '>'?'selected':'').">></option>
+			<option value='=' ".($_POST['COMPAR'] == '='?'selected':'').">=</option>
+		</select><input type='input' name='NBRE' value='".$_POST['NBRE']."' ".$numeric."></td></tr>
+		<tr><td colspan=2 align=center><a href='ipcsv.php'>".$l->g(136)." ".$l->g(765)."</a></td></tr></table>
+		";
 echo "<input type='hidden' id='page' name='page' value=''>";
  echo "</form></table>";
 ?>
