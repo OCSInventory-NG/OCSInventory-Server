e7fb075a0c0f92325a60a6fdb1da8bcd602ec1de
Gonéri Le Bouder <goneri@rulezlan.org>
2009-11-20 22:30:41 +0100

    bzr https access is broken?

--- binutils/create-release-tarball.sh
+++ binutils/create-release-tarball.sh
@@ -9,10 +9,10 @@ if [ -z $RELEASE ]; then
 	exit 1
 fi
 
-bzr branch https://code.launchpad.net/ocsinventory-server/trunk OCSNG_UNIX_SERVER-$RELEASE
+bzr branch lp:ocsinventory-server/trunk OCSNG_UNIX_SERVER-$RELEASE
 rm -rf OCSNG_UNIX_SERVER-$RELEASE/.bzr
 cd OCSNG_UNIX_SERVER-$RELEASE
-bzr branch https://code.launchpad.net/ocsinventory-ocsreports/stable ocsreports
+bzr branch lp:ocsinventory-ocsreports/stable ocsreports
 rm -rf ocsreports/.bzr
 # Reset the default settings so install.php will be correctly loaded
 rm ocsreports/dbconfig.inc.php
