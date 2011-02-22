ff0eb38958b694d1a609e1452730921d0f5c2ddf
mortheres <guillaume.protet@mortheres.info>
2011-02-22 14:44:57 +0100

    Set magic_quotes_gpc to off in etc/ocsinventory-reports.conf to prevent bugs on GUI

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '650';
+$Apache::Ocsinventory::BUILD_VERSION = '651';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
