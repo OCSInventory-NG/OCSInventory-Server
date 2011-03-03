5db78a4fdcae8711c0701d381c6fa677974d21e2
mortheres <guillaume.protet@mortheres.info>
2011-03-03 19:21:15 +0100

    Fix a bug in Download.pm: ERR_ALREADY_IN_HISTORY message was never added in devices table

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '652';
+$Apache::Ocsinventory::BUILD_VERSION = '653';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
