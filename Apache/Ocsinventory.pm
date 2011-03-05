2f27781c203cbab08b22b75c3e3189b4454b34aa
mortheres <guillaume.protet@mortheres.info>
2011-03-05 19:36:27 +0100

    Fix Bug #726522 about OCS_OPT_DBI_MYSQL_SOCKET option in ocsinventory-server.conf Thanks to mar.mack for his report

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '653';
+$Apache::Ocsinventory::BUILD_VERSION = '654';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
