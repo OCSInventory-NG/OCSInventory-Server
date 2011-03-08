ca2a0e9a0f0e5ddf2e9ba112d7e74d363385d7e0
mortheres <guillaume.protet@mortheres.info>
2011-03-09 00:52:23 +0100

    Verify that HARDWARE_ID are not groups when computing groups caches

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '656';
+$Apache::Ocsinventory::BUILD_VERSION = '657';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
