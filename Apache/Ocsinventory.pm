19f3cb241c5bee55047bcbae635b98fb369943b6
mortheres <guillaume.protet@mortheres.info>
2011-03-23 19:32:46 +0100

    Change encoding name to UTF-8 in XML sent by server

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '662';
+$Apache::Ocsinventory::BUILD_VERSION = '663';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
