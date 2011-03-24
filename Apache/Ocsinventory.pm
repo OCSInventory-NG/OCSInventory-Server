7d2cf5868ff789325a3807dfbb147deff0567a50
mortheres <guillaume.protet@mortheres.info>
2011-03-24 11:13:01 +0100

    Preparing for 2.0rc3 release

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '664';
+$Apache::Ocsinventory::BUILD_VERSION = '665';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
