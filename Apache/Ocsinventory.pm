7c73d2cf2aef88561a7519b0231543cc1aeb2064
mortheres <guillaume.protet@mortheres.info>
2011-04-10 19:48:07 +0200

    Add availability to specify mysql socket in ipdiscover-util.pl

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '666';
+$Apache::Ocsinventory::BUILD_VERSION = '667';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
