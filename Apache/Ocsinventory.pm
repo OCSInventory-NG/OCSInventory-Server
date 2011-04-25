24faa6c761af9659522578d4ba3fa67fe8da17c4
mortheres <guillaume.protet@mortheres.info>
2011-04-25 20:24:37 +0200

    Update DTD

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '669';
+$Apache::Ocsinventory::BUILD_VERSION = '670';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
