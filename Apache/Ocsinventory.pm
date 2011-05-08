67271f2d3abfd3a04d1372444690b351b78d9a50
mortheres <guillaume.protet@mortheres.info>
2011-05-08 19:17:54 +0200

    Preparing for 2.0 release Update DTD Fix mistake in Useragent.pm footer

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '673';
+$Apache::Ocsinventory::BUILD_VERSION = '674';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
