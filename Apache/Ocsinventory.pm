276072dc7431d0bb2d09d3f2b9ba7592276835ab
mortheres <guillaume.protet@mortheres.info>
2011-11-02 22:25:17 +0100

    Fix various bugs in NOTIFY mode

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '683';
+$Apache::Ocsinventory::BUILD_VERSION = '684';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
