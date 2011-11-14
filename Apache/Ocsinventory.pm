ebf267cd821ed64364fe35ea94ddbb9e99f8f43c
mortheres <guillaume.protet@mortheres.info>
2011-11-14 11:03:13 +0100

    Enhance https control in Snmp.pm

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.0';
-$Apache::Ocsinventory::BUILD_VERSION = '686';
+$Apache::Ocsinventory::BUILD_VERSION = '687';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
