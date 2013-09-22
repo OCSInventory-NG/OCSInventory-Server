3fc8cbddc24938c23b8f0388c5ae9ed1fe6135b5
mortheres <guillaume.protet@mortheres.info>
2013-09-22 19:47:59 +0200

    Fix Bug #1191791 about CHECKSUM value not updated when NOTIFY mode send new network changes

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.1';
-$Apache::Ocsinventory::BUILD_VERSION = '711';
+$Apache::Ocsinventory::BUILD_VERSION = '712';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
