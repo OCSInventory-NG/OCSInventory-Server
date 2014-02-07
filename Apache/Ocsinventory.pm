28d9c0bad3e3015e12714cd2ae3c150f9e897116
mortheres <guillaume.protet@mortheres.info>
2014-02-07 14:51:15 +0100

    Fix a mistake for OCS_PLUGINS_PERL_DIR and OCS_PLUGINS_CONF_DIR options in ocsinventory-server.conf

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.1';
-$Apache::Ocsinventory::BUILD_VERSION = '722';
+$Apache::Ocsinventory::BUILD_VERSION = '723';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
