09edcdf71e3bf1899505d5719efccd59cad1b78b
mortheres <guillaume.protet@mortheres.info>
2014-05-05 16:19:27 +0200

    Fix Bug #1311852 about CHECKSUM updated for all computers

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.1';
-$Apache::Ocsinventory::BUILD_VERSION = '723';
+$Apache::Ocsinventory::BUILD_VERSION = '724';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
