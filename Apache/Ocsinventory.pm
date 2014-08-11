eebb798ff03c4d02d9eae8ff89659f93d6e34439
mortheres <guillaume.protet@mortheres.info>
2014-08-11 16:53:04 +0200

    Fix bug in SOAP engine while retrieving data from type tables

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -28,7 +28,7 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.1';
-$Apache::Ocsinventory::BUILD_VERSION = '728';
+$Apache::Ocsinventory::BUILD_VERSION = '729';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
 # Ocs modules
