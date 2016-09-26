3122cf2e5e76d0f90cd219ebed22e93d38392d05
Gilles Dubois <gilles.dubois@factorfx.com>
2016-09-26 17:36:07 +0200

    Add error return code on webservice

--- Apache/Ocsinventory/Plugins/Modules.pm
+++ Apache/Ocsinventory/Plugins/Modules.pm
@@ -20,74 +20,98 @@ use DBI;
   
 sub InstallPlugins {
 
-my $pluginName = $_[1];
+    my $pluginName = $_[1];
 
-# Download the created archive from the ocsreports which contain the communication server code (.conf and map.pm)
-my $url = "http://$ENV{OCS_DB_HOST}/ocsreports/upload/$pluginName.zip";
-my $file = "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName.zip";
-our $test;
-	
-if (-e "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName") {
-	$test = 1;
-	print "Archive existante";
-}
-else
-{
-	
-	print "$url\n";
-	
-	my $status = getstore($url, $file);
+    # Download the created archive from the ocsreports which contain the communication server code (.conf and map.pm)
+    my $url = "http://$ENV{OCS_DB_HOST}/ocsreports/upload/$pluginName.zip";
+    my $file = "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName.zip";
+    our $result;
+    our $perm = 1;
 
-	# If download succes, unzip, create dir, move files.
-	if (is_success($status))
-	{
-		my $pluginsdir = "$ENV{OCS_PLUGINS_CONF_DIR}";
-		my $zipname = $file;
-		my $destinationDirectory = $pluginsdir;
-		my $zip = Archive::Zip->new($zipname);
-		my $member;
-				
-		foreach my $member ($zip->members)
-		{
-			next if $member->isDirectory;
-			(my $extractName = $member->fileName) =~ s{.*/}{};
-			$member->extractToFileNamed("$destinationDirectory/$extractName");
-		}
-		
-		#Up case plugin directory in OCS server for match with actual template
-		my $pluginNameUc = ucfirst($pluginName);
-		
-		my $dirtocreate = "$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins/$pluginNameUc";
-		mkdir $dirtocreate;
-			
-		unlink $file;
-		move("$pluginsdir/Map.pm","$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins/$pluginNameUc/Map.pm");
-	}
-}
+    #Up case plugin directory in OCS server for match with actual template
+    our $pluginNameUc = ucfirst($pluginName);
 
-my $result = "Install OK";
-return( SOAP::Data->name( 'Result' => $result )->type( 'string' ) );
+    if (-e "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName.conf") {
+        $result = "Err_01";
+    }
+    elsif(-e "$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins/$pluginNameUc"){
+        $result = "Err_05";
+    }
+    else
+    {
+
+        print "$url\n";
+
+        my $status = getstore($url, $file);
+
+        # If download succes, unzip, create dir, move files.
+        if (is_success($status))
+        {
+
+            # Check for write perm in plugins dir
+            if(!(-w "$ENV{OCS_PLUGINS_CONF_DIR}"))
+            {
+                $result = "Err_03";
+                $perm = 0;
+            }
+            # Check for write perm in perl dir
+            if(!(-w "$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins"))
+            {
+                $result = "Err_04";
+                $perm = 0;
+            }
+
+            if($perm){
+                my $pluginsdir = "$ENV{OCS_PLUGINS_CONF_DIR}";
+                my $zipname = $file;
+                my $destinationDirectory = $pluginsdir;
+                my $zip = Archive::Zip->new($zipname);
+                my $member;
+
+                foreach my $member ($zip->members)
+                {
+                        next if $member->isDirectory;
+                        (my $extractName = $member->fileName) =~ s{.*/}{};
+                        $member->extractToFileNamed("$destinationDirectory/$extractName");
+                }
+
+                my $dirtocreate = "$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins/$pluginNameUc";
+                mkdir $dirtocreate;
+
+                unlink $file;
+                move("$pluginsdir/Map.pm","$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins/$pluginNameUc/Map.pm");
+
+                $result = "Install_OK";
+            }
+
+        }else{
+            $result = "Err_02";
+        }
+
+    }
+
+    return( SOAP::Data->name( 'Result' => $result )->type( 'string' ) );
 
 }
 
 # Seek for deleted plugins // Delete map.pm and conf entry.
-
 sub DeletePlugins {
 	
-	my $pluginName = $_[1];
-	
-	#Up case plugin directory in OCS server for match with actual template for deletion
-	my $pluginNameUc = ucfirst($pluginName);
-	
-	my $pluginsdir = "$ENV{OCS_PLUGINS_CONF_DIR}";
-	
-	if (-e "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName.conf"){
-		unlink "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName.conf";
-	}
-	
-	rmtree "$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins/$pluginNameUc";
-	
-	my $result = "Delete OK";
+    my $pluginName = $_[1];
+
+    #Up case plugin directory in OCS server for match with actual template for deletion
+    my $pluginNameUc = ucfirst($pluginName);
+
+    my $pluginsdir = "$ENV{OCS_PLUGINS_CONF_DIR}";
+
+    if (-e "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName.conf"){
+            unlink "$ENV{OCS_PLUGINS_CONF_DIR}/$pluginName.conf";
+    }
+
+    rmtree "$ENV{OCS_PLUGINS_PERL_DIR}/Apache/Ocsinventory/Plugins/$pluginNameUc";
+
+    my $result = "Delete_OK";
+
     return( SOAP::Data->name( 'Result' => $result )->type( 'string' ) );
 }
 
