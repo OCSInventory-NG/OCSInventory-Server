d2486426ff52a74a5c51ec14ebcc39f576d3a051
Gilles Dubois <gilles.dubois@factorfx.com>
2016-12-15 11:20:20 +0100

    Add script for docker

---
+++ binutils/docker-download.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Define constant
+
+# Github base url for download releases
+ReleaseBaseUrl="https://github.com/OCSInventory-NG/OCSInventory-ocsreports/releases/download/"
+# Last release tag
+LastReleaseTag="2.3RC1/"
+# Archive name
+LastReleaseArchive="OCSNG_UNIX_SERVER-2.3RC"
+# Archive extension
+ArchiveExtension=".tar.gz"
+# File destination
+FileDestination="/tmp/ocs"
+# Archive link for release
+FullArchiveUrl=$ReleaseBaseUrl$LastReleaseTag$LastReleaseArchive$ArchiveExtension
+
+# Get archive
+wget $FullArchiveUrl
+
+# Un tar release archive
+tar -xzvf $LastReleaseArchive$ArchiveExtension
+
+# Move to the selected directory
+mv $LastReleaseArchive $FileDestination
