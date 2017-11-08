d1c2f811950969140e664ad5acca78a69afb4914
Gilles Dubois <gilles.dubois@factorfx.com>
2017-11-08 14:24:40 +0100

    Update API route declaration order

--- Api/Ocsinventory/Restapi/Loader.pm
+++ Api/Ocsinventory/Restapi/Loader.pm
@@ -24,6 +24,11 @@ require Api::Ocsinventory::Restapi::Snmp::Get::SnmpListId; # Get list of Snmp ID
 
 ## Routes
 
+get '/v1/computers/listID' => sub {
+        my $c = shift;
+        $c->render(data => Api::Ocsinventory::Restapi::Computer::Get::ComputersListId::get_computers_id());
+};
+
 get '/v1/computer/:id' => sub {
         my $c = shift;
         my $id = $c->stash('id');
@@ -47,12 +52,6 @@ get '/v1/computers' => sub {
         $c->render(data => Api::Ocsinventory::Restapi::Computer::Get::Computers::get_computers($limit, $start));
 };
 
-
-get '/v1/computers/listID' => sub {
-        my $c = shift;
-        $c->render(data => Api::Ocsinventory::Restapi::Computer::Get::ComputersListId::get_computers_id());
-};
-
 get '/v1/ipdiscover' => sub {
         my $c = shift;
 
@@ -67,6 +66,11 @@ get '/v1/ipdiscover/:network' => sub {
         my $network = $c->stash('network');
 
         $c->render(data => Api::Ocsinventory::Restapi::Ipdiscover::Get::IpdiscoverNetwork::get_ipdiscover_network($network));
+};
+
+get '/v1/snmp/listID' => sub {
+        my $c = shift;
+        $c->render(data => Api::Ocsinventory::Restapi::Snmp::Get::SnmpListId::get_snmps_id());
 };
 
 get '/v1/snmp' => sub {
@@ -90,11 +94,6 @@ get '/v1/snmp/:id/:field' => sub {
         my $id = $c->stash('id');
         my $field = $c->stash('field');
         $c->render(data => Api::Ocsinventory::Restapi::Snmp::Get::SnmpIdField::get_snmp_field($id, $field));
-};
-
-get '/v1/snmp/listID' => sub {
-        my $c = shift;
-        $c->render(data => Api::Ocsinventory::Restapi::Snmp::Get::SnmpListId::get_snmps_id());
 };
 
 app->start;
