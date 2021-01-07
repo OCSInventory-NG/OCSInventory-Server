468a4317108c68140da9a836290e6e1f3f1bbdbd
Gilles Dubois <gilles.dubois@factorfx.com>
2021-01-07 17:30:53 +0100

    Add route definition and import

--- Api/Ocsinventory/Restapi/Loader.pm
+++ Api/Ocsinventory/Restapi/Loader.pm
@@ -30,6 +30,11 @@ require Api::Ocsinventory::Restapi::Snmp::Get::SnmpId; # Get specific Snmp type 
 require Api::Ocsinventory::Restapi::Snmp::Get::SnmpIdField; # Get specific information by id of Snmp type
 require Api::Ocsinventory::Restapi::Snmp::Get::SnmpListId; # Get list of Snmp Type
 
+## CVE section
+require Api::Ocsinventory::Restapi::Cve::Get::CveCvss;
+require Api::Ocsinventory::Restapi::Cve::Get::CveSoftware;
+require Api::Ocsinventory::Restapi::Cve::Get::CveComputer;
+
 ## Routes
 
 get '/v1/computers/listID' => sub {
@@ -127,6 +132,33 @@ get '/v1/snmp/:type/:id' => sub {
         my $type = $c->stash('type');
         my $id = $c->stash('id');
         $c->render(json => Api::Ocsinventory::Restapi::Snmp::Get::SnmpIdField::get_snmp_field($type, $id));
+};
+
+get '/v1/cve/cvss' => sub {
+        my $c = shift;
+
+        my $start = $c->param('start')||0;
+        my $limit = $c->param('limit')||0;
+
+        $c->render(json => Api::Ocsinventory::Restapi::Cve::Get::CveCvss::get_cve_cvss($start, $limit));
+};
+
+get '/v1/cve/software' => sub {
+        my $c = shift;
+
+        my $start = $c->param('start')||0;
+        my $limit = $c->param('limit')||0;
+
+        $c->render(json => Api::Ocsinventory::Restapi::Cve::Get::CveSoftware::get_cve_software($start, $limit));
+};
+
+get '/v1/cve/computer' => sub {
+        my $c = shift;
+
+        my $start = $c->param('start')||0;
+        my $limit = $c->param('limit')||0;
+
+        $c->render(json => Api::Ocsinventory::Restapi::Cve::Get::CveComputer::get_cve_computer($start, $limit));
 };
 
 app->start;
