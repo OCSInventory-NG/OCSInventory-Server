406e62fe437c8b5a0db94744b5485d60a80198e2
Charlene Auger <charlene.auger@factorfx.com>
2021-02-15 17:29:50 +0100

    Update

--- Apache/Ocsinventory/Server/Inventory/Software.pm
+++ Apache/Ocsinventory/Server/Inventory/Software.pm
@@ -190,9 +190,7 @@ sub _insert_software {
     foreach my $software (@{$Apache::Ocsinventory::CURRENT_CONTEXT{'XML_ENTRY'}->{CONTENT}->{SOFTWARES}}) {
 
         # Check install date format
-        if($software->{INSTALLDATE} =~ /^\d{4}\/\d\d\/\d\d$/ || $software->{INSTALLDATE} =~ /[1-9]{1}[0-9]{3}\/[0-9]{2}\/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/) {
-            # Do nothing
-        } else {
+        if($software->{INSTALLDATE} !~ /^\d{4}\/\d\d\/\d\d$/ || $software->{INSTALLDATE} !~ /[1-9]{1}[0-9]{3}\/[0-9]{2}\/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/) {
             $software->{INSTALLDATE} = strftime "%Y/%m/%d", localtime;
         }
 
