8e4a84184dc726f7cf5ca9a3fc57e7b57a0a7936
Charlène Auger <charlene.auger@factorfx.com>
2021-09-24 10:46:20 +0200

    Add snmpv3 parameters to prolog

--- Apache/Ocsinventory/Server/Capacities/Snmp.pm
+++ Apache/Ocsinventory/Server/Capacities/Snmp.pm
@@ -181,7 +181,7 @@ sub snmp_prolog_resp{
 
       if (@snmp) {
         #Getting snmp communities
-        $select_communities_req = $dbh->prepare('SELECT VERSION,NAME,USERNAME,AUTHKEY,AUTHPASSWD,AUTHPROTO,PRIVPROTO FROM snmp_communities');
+        $select_communities_req = $dbh->prepare('SELECT VERSION,NAME,USERNAME,AUTHKEY,AUTHPASSWD,LEVEL,AUTHPROTO,PRIVPASSWD,PRIVPROTO FROM snmp_communities');
         $select_communities_req->execute();
 
         while(my $row = $select_communities_req->fetchrow_hashref){
@@ -196,7 +196,9 @@ sub snmp_prolog_resp{
               'USERNAME'=> $community->{'USERNAME'}?$community->{'USERNAME'}:'',
               'AUTHKEY' => $community->{'AUTHKEY'}?$community->{'AUTHKEY'}:'',
               'AUTHPASSWD' => $community->{'AUTHPASSWD'}?$community->{'AUTHPASSWD'}:'',
+              'LEVEL'=> $community->{'LEVEL'}?$community->{'LEVEL'}:'',
               'AUTHPROTO' => $community->{'AUTHPROTO'}?$community->{'AUTHPROTO'}:'',
+              'PRIVPASSWD' => $community->{'PRIVPASSWD'}?$community->{'PRIVPASSWD'}:'',
               'PRIVPROTO' => $community->{'PRIVPROTO'}?$community->{'PRIVPROTO'}:'',
               'TYPE' => 'COMMUNITY',
             };
