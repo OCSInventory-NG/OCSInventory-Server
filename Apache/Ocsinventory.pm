17d0a045085460577baab1766f850a21213d2242
Charlene Auger <charlene.auger@factorfx.com>
2022-06-21 15:53:25 +0200

    Add major and minor version and compare with major and minor useragent version

--- Apache/Ocsinventory.pm
+++ Apache/Ocsinventory.pm
@@ -40,6 +40,8 @@ BEGIN{
 }
 
 $Apache::Ocsinventory::VERSION = '2.10.0';
+$Apache::Ocsinventory::MAJOR_VERSION = '2';
+$Apache::Ocsinventory::MINOR_VERSION = '10';
 $Apache::Ocsinventory::BUILD_VERSION = '820';
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
